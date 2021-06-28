4177d7799c2b05e24c56872979f83d793490e835
john paul <johnjeneiro@gmail.com>
2021-06-28 09:11:18 +0100

    mm

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.use(cors({ origin: ["http://localhost:3000"], credentials: true }));
+app.use(cors({ origin: ["https://apexwavefintech.netlify.app"], credentials: true }));
 app.use
 
 //app.get("/test", (req, res)=>{ res.send("it works!")})
