dd7dad54a6219bc07f3cff4f095718e87da2a880
John Jeneiro <johnjeneiro@gmail.com>
2021-12-03 19:49:14 +0100

    Update investmentModel.js

--- models/investmentModel.js
+++ models/investmentModel.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const investmentSchema = new mongoose.Schema({
   fullName: { type: String, required: true },
   amount: { type: Number, required: true },
-  referrer: { type: String, required: true },
+  referrer: { type: String },
   status: {
     type: String,
     enum: ["Pending", "Payment Confirmed"],
